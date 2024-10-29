4270d60b49833aedf4cd9080f4f7648dca30234c
Katerina Kalaydzhieva <pmg20924@hotmail.com>
2024-10-29 16:43:12 +0200

    modified the roles

--- LearnSpace.Infrastructure/Database/Entities/Account/Student.cs
+++ LearnSpace.Infrastructure/Database/Entities/Account/Student.cs
@@ -1,9 +1,17 @@
-﻿namespace LearnSpace.Infrastructure.Database.Entities.Account
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearnSpace.Infrastructure.Database.Entities.Account
 {
-    public class Student : ApplicationUser
+    public class Student
     {
+        [Key]
+        public int Id { get; set; }
         public IEnumerable<Grade> Grades { get; set; } = new List<Grade>();
         public IEnumerable<StudentCourse> StudentCourses { get; set; } = new List<StudentCourse>();
-    }
 
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser User { get; set; } = null!;
+    }
 }
