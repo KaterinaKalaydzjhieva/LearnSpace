d0697794929099487d200b07786b9011e38d9484
KaterinaKalaydzjhieva <katqkalaidjieva@gmail.com>
2024-11-30 22:41:14 +0200

    added a chart to dashboard

--- LearnSpace.Core.Models/Student/StudentDashboardModel.cs
+++ LearnSpace.Core.Models/Student/StudentDashboardModel.cs
@@ -8,5 +8,16 @@
         public int AssignmentCount { get; set; }
         public int ClassCount { get; set; }
 
+        //public List<string> GetLastSixMonths() 
+        //{
+        //    var dateNow = DateTime.Now;
+        //    var list = new List<string>();
+
+        //    for (int i = 0; i < GradeCount; i++) 
+        //    {
+            
+        //    }
+        //}
+
     }
 }
